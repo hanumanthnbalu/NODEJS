b9346b06663ea2d187748dad1720f037aa1bc91a
Hanumanthnbalu <hanumanthnbalu@gmail.com>
2019-03-26 21:26:28 +0530

    update

---
+++ public/js/adimn.js
@@ -0,0 +1,4 @@
+const deleteProduct = () => {
+   const prodId =  btn.parentnode.querySelector('[name=productId').value;
+   const csrf = btn.parentnode.querySelector('[name=_csrf').value;
+};
---
+++ util/database.js
@@ -0,0 +1,30 @@
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+
+// let _db;
+
+// const mongoConnect = callback => {
+//   MongoClient.connect(
+//     'mongodb+srv://hnbalu:hnbalu123@nodejs-bgxcz.mongodb.net/Node_Shop?retryWrites=true',
+//     { useNewUrlParser: true }
+//   )
+//     .then(client => {
+//       console.log('Connected to MONGODB!');
+//       _db = client.db();
+//       callback();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       throw err;
+//     });
+// };
+
+// const getDb = () => {
+//   if (_db) {
+//     return _db;
+//   }
+//   throw 'No database found!';
+// };
+
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb;
